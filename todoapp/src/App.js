36740282b2f1b798293c82994b8ea0f1085c02ba
unknown <v_montanez@hotmail.com>
2020-05-29 23:51:45 -0400

    made class extends component method & added render method

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React , { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render () {
   return (
     <div className="App">
       My Hello World
     </div>
   );
 }
-
+}
 export default App;
