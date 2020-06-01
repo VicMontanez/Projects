91d939eb2062e822835a2dd2ef176cdb9875ecc5
unknown <v_montanez@hotmail.com>
2020-05-31 23:34:24 -0400

    made generic event that handles all changes

--- todoapp/src/components/todo/TodoApp.jsx
+++ todoapp/src/components/todo/TodoApp.jsx
@@ -22,21 +22,26 @@ class LoginComponent extends Component {
         }
         this.handleUsernameChange = this.handleUsernameChange.bind(this)
         this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    handleUsernameChange(event) {
+    handleChange(event) {
         console.log(event.target.value);
-        this.setState({username: event.target.value})
+        this.setState(
+            {
+                [event.target.name]: event.target.value
+            })
     }
 
-    handlePasswordChange(event) {
-        this.setState({password: event.target.value})
-    }
+    // handlePasswordChange(event) {
+    //     this.setState({password: event.target.value})
+    // }
+
     render() {
         return (
             <div>
-            User Name: <input type="text" nane="username" value={this.state.username} onChange={this.handleUsernameChange}/>
-            Password: <input type="password" name="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+            Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
             <button>Login</button>
            </div>
         )
