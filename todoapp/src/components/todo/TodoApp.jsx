1ead8a789806542ea661da553738e4d3e021cd65
unknown <v_montanez@hotmail.com>
2020-06-01 01:00:50 -0400

    created functions for login failure or login success

--- todoapp/src/components/todo/TodoApp.jsx
+++ todoapp/src/components/todo/TodoApp.jsx
@@ -56,12 +56,29 @@ class LoginComponent extends Component {
     render() {
         return (
             <div>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/>
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/>
             User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
             Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
             <button onClick={this.loginClicked}>Login</button>
            </div>
         )
     }
+
+}
+
+function ShowInvalidCredentials(props) {
+      if(props.hasLoginFailed) {
+          return <div>Invalid Credentials</div>
+      }  
+      return null
+}
+
+function ShowLoginSuccessMessage(props) {
+    if (props.showSuccessMessage) {
+        return <div>Success!</div>
+    }
+    return null
 }
 
 export default TodoApp
