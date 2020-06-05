bfd6f03bd3f2600cd8d3bb4144a59500d1339874
unknown <v_montanez@hotmail.com>
2020-06-05 00:18:23 -0400

    added list

--- todoapp/src/components/todo/TodoApp.jsx
+++ todoapp/src/components/todo/TodoApp.jsx
@@ -23,8 +23,30 @@ class TodoApp extends Component {
 }
 
 class ListTodosComponent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            todo: {id: 1, description: 'Learn React'}
+        }
+    }
     render() {
-        return <div>List Todos</div>
+        return <div>
+            <h1>List Todos</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>{this.state.todo.id}</td>
+                    <td>{this.state.todo.description}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     }
 }
 
