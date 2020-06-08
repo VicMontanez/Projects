b8d33a6e463c5f7ccdd479c355f8219c40595c88
unknown <v_montanez@hotmail.com>
2020-06-07 23:04:04 -0400

    added link to for welcome page

--- todoapp/src/components/todo/TodoApp.jsx
+++ todoapp/src/components/todo/TodoApp.jsx
@@ -31,7 +31,7 @@ class HeaderComponent extends Component {
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div><a href="google.com" className="navbar-brand">Todo App</a></div>
                     <ul className="navbar-nav">
-                        <li className="nav-link">Home</li>
+                        <li className="nav-link"><Link to="/welcome">Home</Link></li>
                         <li className="nav-link">Todos</li>
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
