11820f009c8b556402217975db82dadf4024bdad
unknown <v_montanez@hotmail.com>
2020-06-08 19:30:47 -0400

    styled login component

--- todoapp/src/components/MusicApp/MusicApp.jsx
+++ todoapp/src/components/MusicApp/MusicApp.jsx
@@ -169,13 +169,16 @@ class MusicApp extends Component {
     render() {
         return (
             <div>
-                {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
-                {this.state.hasLoginFailed && <div>Invalid Credentials</div>}
-                {this.state.showSuccessMessage && <div>Login Successful!</div>}
-                {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/> */}
-            User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
-            Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
-            <button onClick={this.loginClicked}>Login</button>
+                <h1>Login</h1>
+                <div className="container">
+                    {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
+                    {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+                    {this.state.showSuccessMessage && <div>Login Successful!</div>}
+                    {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/> */}
+                    User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                    Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                    <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
+                </div>
            </div>
         )
     }
