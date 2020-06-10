560f45cfb7dd0674a70bddbfbff395cb89040f38
unknown <v_montanez@hotmail.com>
2020-06-09 23:05:11 -0400

    moved login component to its own seperate file

---
+++ todoapp/src/components/MusicApp/LoginComponent.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import AuthenticationService from './AuthenticationService'
+
+
+class LoginComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: 'username',
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
+        }
+        //this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        //this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.loginClicked = this.loginClicked.bind(this)
+    }
+
+    handleChange(event) {
+       // console.log(event.target.value);
+        this.setState(
+            {
+                [event.target.name]: event.target.value
+            })
+    }
+
+    // handlePasswordChange(event) {
+    //     this.setState({password: event.target.value})
+    // }
+
+    loginClicked() {
+        if(this.state.username === "username" && this.state.password=== "password1234"){
+            AuthenticationService.registerSuccessfulLogin()
+            this.props.history.push(`/welcome/${this.state.username}`);
+            //this.setState({showSuccessMessage:true})
+            //this.setState({hasLoginFailed:false})
+        }
+        else {
+            this.setState({showSuccessMessage:false})
+            this.setState({hasLoginFailed:true})
+        }
+        
+        //console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Login</h1>
+                <div className="container">
+                    {/* <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed}/> */}
+                    {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+                    {this.state.showSuccessMessage && <div>Login Successful!</div>}
+                    {/* <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage}/> */}
+                    User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                    Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                    <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
+                </div>
+           </div>
+        )
+    }
+
+}
+
+export default LoginComponent
