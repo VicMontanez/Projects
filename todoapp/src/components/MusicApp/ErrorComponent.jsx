ca55d69aa189ccad47ce35fb1a9ee78af485199c
unknown <v_montanez@hotmail.com>
2020-06-10 22:37:39 -0400

    moved error component to seperate file

---
+++ todoapp/src/components/MusicApp/ErrorComponent.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+ // function 
+ function ErrorComponent() {
+    return <div>An Error Occured. I don't know what to do! Contact support at (555)555-5555</div>
+  }
+
+  export default ErrorComponent
