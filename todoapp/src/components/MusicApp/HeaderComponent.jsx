91327e547b54c93f3655986f80a762229ed5631a
unknown <v_montanez@hotmail.com>
2020-06-10 22:45:19 -0400

    moved header component to seperate file

---
+++ todoapp/src/components/MusicApp/HeaderComponent.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AuthenticationService from './AuthenticationService.js';
+
+class HeaderComponent extends Component {
+    render() {
+        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+        //console.log(isUserLoggedIn)
+        return (
+            <header>
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                    <div><a href="" className="navbar-brand">Music App</a></div>
+                    <ul className="navbar-nav">
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/">Home</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/about">About us</Link></li>}
+                    </ul>
+                    <ul className="navbar-nav navbar-collapse justify-content-end">
+                        {!isUserLoggedIn && <li><Link className="nav-link" to="/login">Login</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link"to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>}
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default HeaderComponent
