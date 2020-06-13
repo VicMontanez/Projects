5f507e6014b0d27316beac5c2712e4411df3aff3
unknown <v_montanez@hotmail.com>
2020-06-12 21:07:05 -0400

    added constructor for retrieve welcome message

--- todoapp/src/components/MusicApp/WelcomeComponent.jsx
+++ todoapp/src/components/MusicApp/WelcomeComponent.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class WelcomeComponent extends Component {
- 
+ constructor(props){
+     super(props)
+     this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
+ }
     render() {
         return (
             <>
@@ -12,11 +15,16 @@ class WelcomeComponent extends Component {
                     You can search music <Link to="/search">here</Link>
                 </div>
                 <div>
-                    Click here to get a customized message. <button onClick={this.retrieveWelcomeMessage}>Get Welcome Message</button>
+                    Click here to get a customized message. 
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
 
                 </div>
                 </>
                 )
+        }
+
+        retrieveWelcomeMessage() {
+            console.log('retrieved click')
         }
     }
 
