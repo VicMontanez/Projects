1e03b29b9b6d62df3f498a61fc282fcde0fde446
unknown <v_montanez@hotmail.com>
2020-06-12 21:01:03 -0400

    added button to display message

--- todoapp/src/components/MusicApp/WelcomeComponent.jsx
+++ todoapp/src/components/MusicApp/WelcomeComponent.jsx
@@ -11,6 +11,10 @@ class WelcomeComponent extends Component {
                     Welcome <b>{this.props.match.params.name}</b> to the Music API.<br></br>
                     You can search music <Link to="/search">here</Link>
                 </div>
+                <div>
+                    Click here to get a customized message. <button onClick={this.retrieveWelcomeMessage}>Get Welcome Message</button>
+
+                </div>
                 </>
                 )
         }
