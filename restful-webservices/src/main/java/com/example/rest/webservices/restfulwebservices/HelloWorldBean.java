54bc72ab8d91a487fffd3285f3cb13eac4e0a97b
unknown <v_montanez@hotmail.com>
2020-06-18 00:48:30 -0400

    created setter for bean

---
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,17 @@
+package com.example.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		// TODO Auto-generated constructor stub
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+}
