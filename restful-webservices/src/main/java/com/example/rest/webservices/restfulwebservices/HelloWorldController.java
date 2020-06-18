40b544e6ff8252d86f6e77c562934445439a8795
unknown <v_montanez@hotmail.com>
2020-06-18 01:06:27 -0400

    added a bean to controller

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	//GET
-	//URI - /hello-world
-	//method -"Hello World"
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+		
+	}
+	
+	//hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 		
 	}
 }
