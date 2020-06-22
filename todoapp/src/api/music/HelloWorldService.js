b52ef36d38e87b932399fd9ff5b93fd284cd93e7
unknown <v_montanez@hotmail.com>
2020-06-22 01:06:34 -0400

    made method for hello world bean

--- todoapp/src/api/music/HelloWorldService.js
+++ todoapp/src/api/music/HelloWorldService.js
@@ -1,9 +1,16 @@
+import Axios from "axios"
+import axios from 'axios'
 
 class HelloWorldService {
     executeHelloWorldService() {
-        console.log('executed service')
+       return axios.get('http://localhost:8080/hello-world');
+        //console.log('executed service')
     }
 
+    executeHelloWorldBeanService() {
+        return axios.get('http://localhost:8080/hello-world-bean');
+         //console.log('executed service')
+     }
 
 }
 
