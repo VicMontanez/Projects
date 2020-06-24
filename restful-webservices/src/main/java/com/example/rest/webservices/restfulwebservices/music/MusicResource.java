924ce993ac4c92b0f4bf422d0e260bc99a72269c
unknown <v_montanez@hotmail.com>
2020-06-23 23:28:21 -0400

    added get mapping

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
@@ -3,6 +3,7 @@ package com.example.rest.webservices.restfulwebservices.music;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ public class MusicResource {
 	@Autowired
 	private MusicHardcodedService musicService;
 	
+	@GetMapping("users/{username}/music")
 	public List<Music> getAllMusic(@PathVariable String username){
 		return musicService.findAll();
 	}
