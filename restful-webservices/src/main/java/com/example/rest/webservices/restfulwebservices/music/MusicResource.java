9c1dd495974c0d75bddfe8f4ab713c7149c26cb6
unknown <v_montanez@hotmail.com>
2020-06-23 23:26:50 -0400

    added path variable

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
@@ -3,6 +3,7 @@ package com.example.rest.webservices.restfulwebservices.music;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +12,7 @@ public class MusicResource {
 	@Autowired
 	private MusicHardcodedService musicService;
 	
-	public List<Music> getAllMusic(String username){
+	public List<Music> getAllMusic(@PathVariable String username){
 		return musicService.findAll();
 	}
 }
