cf218b9d4adae94cd38c45ec8fa5c2c5a4ea4718
unknown <v_montanez@hotmail.com>
2020-06-23 23:10:56 -0400

    added method to return list

---
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicHardcodedService.java
@@ -0,0 +1,24 @@
+package com.example.rest.webservices.restfulwebservices.music;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MusicHardcodedService {
+	
+	private static List<Music> music = new ArrayList<Music>();
+	private static int idCounter = 0;
+	
+	static {
+		music.add(new Music(++idCounter, "username","Drake", new Date(), false ));
+		music.add(new Music(++idCounter, "username","Snoop Dogg", new Date(), false ));
+		music.add(new Music(++idCounter, "username","Jay-z", new Date(), false ));
+	}
+	
+	public List<Music> findAll() {
+		return music;
+		
+	}
+}
+
+// vid 11 6:48
