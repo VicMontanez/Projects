3bed0fa97f68a73d726be255df3caef4c7f32ec2
unknown <v_montanez@hotmail.com>
2020-06-23 23:25:00 -0400

    added return statement

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
@@ -2,14 +2,16 @@ package com.example.rest.webservices.restfulwebservices.music;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MusicResource {
 	
+	@Autowired
 	private MusicHardcodedService musicService;
 	
 	public List<Music> getAllMusic(String username){
-		
+		return musicService.findAll();
 	}
 }
