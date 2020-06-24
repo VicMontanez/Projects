acdb47b92bf213a54f39d6c5278b142f9714169e
unknown <v_montanez@hotmail.com>
2020-06-23 23:22:25 -0400

    added auto wired annotation

---
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
@@ -0,0 +1,15 @@
+package com.example.rest.webservices.restfulwebservices.music;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MusicResource {
+	
+	private MusicHardcodedService musicService;
+	
+	public List<Music> getAllMusic(String username){
+		
+	}
+}
