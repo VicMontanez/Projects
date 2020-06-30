72db653f4bae2878a888f9efde4ae20deac976c0
unknown <v_montanez@hotmail.com>
2020-06-30 01:09:13 -0400

    created response entity for delete request

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicResource.java
@@ -3,7 +3,9 @@ package com.example.rest.webservices.restfulwebservices.music;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +20,16 @@ public class MusicResource {
 	@GetMapping("users/{username}/music")
 	public List<Music> getAllMusic(@PathVariable String username){
 		return musicService.findAll();
+	}
+	
+	@DeleteMapping("users/{username}/music/{id}")
+	public ResponseEntity<Void> deleteMusic(@PathVariable String username, @PathVariable long id){
+		
+		Music artist = musicService.deletebyId(id);
+		if(artist!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
