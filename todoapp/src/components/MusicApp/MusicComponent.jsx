40a3a0c08af469ab9f2002816582705f86702689
unknown <v_montanez@hotmail.com>
2020-07-01 20:42:03 -0400

    added constructor and state

--- todoapp/src/components/MusicApp/MusicComponent.jsx
+++ todoapp/src/components/MusicApp/MusicComponent.jsx
@@ -1,6 +1,16 @@
 import React , {Component} from 'react'
 
 class MusicComponent extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            id: 1,
+            description: 'Learn Forms',
+            targetDate: new Date()
+        }
+    }
+    
     render() {
         return <div>Music Component for id - {this.props.match.params.id}</div>
     }
