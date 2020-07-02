c4d2d6af312546f9587992bedda9a4275f22a8f0
unknown <v_montanez@hotmail.com>
2020-07-01 21:05:33 -0400

    added formik and moment to component

--- todoapp/src/components/MusicApp/MusicComponent.jsx
+++ todoapp/src/components/MusicApp/MusicComponent.jsx
@@ -1,4 +1,6 @@
 import React , {Component} from 'react'
+import moment from 'moment'
+import { Formik } from 'formik'
 
 class MusicComponent extends Component {
     constructor(props) {
@@ -7,12 +9,25 @@ class MusicComponent extends Component {
         this.state = {
             id: 1,
             description: 'Learn Forms',
-            targetDate: new Date()
+            targetDate: moment(new Date()).format('YYYY-MM-DD')
         }
     }
     
     render() {
-        return <div>Music Component for id - {this.props.match.params.id}</div>
+        return <div>
+            <h1>Music</h1>
+            <div class="container">
+                <Formik>
+                    {
+                        (props) => (
+                            <div>Something</div>
+                        )
+                    }
+                </Formik>
+            </div>
+            Music Component for id - {this.props.match.params.id}
+            
+            </div>
     }
 }
 
