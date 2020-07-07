c1be9dc1f8a76ca37a4b101fb71f70672f31a87e
unknown <v_montanez@hotmail.com>
2020-07-07 19:05:27 -0400

    displayed error message to the front end

--- todoapp/src/components/MusicApp/MusicComponent.jsx
+++ todoapp/src/components/MusicApp/MusicComponent.jsx
@@ -45,6 +45,7 @@ class MusicComponent extends Component {
                     {
                         (props) => (
                             <Form>
+                                 <ErrorMessage name="description" component="div" className="alert alert-warning"/>
                                 <fieldset className="form-group">
                                     <label>Description</label>
                                     <Field className="form-control" type="text" name="description"/>
