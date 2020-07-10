34c8799f99710db4cf27b9db4911921a9c93400d
unknown <v_montanez@hotmail.com>
2020-07-10 14:39:12 -0400

    added headers to path variable service

--- todoapp/src/api/music/HelloWorldService.js
+++ todoapp/src/api/music/HelloWorldService.js
@@ -13,7 +13,18 @@ class HelloWorldService {
      }
     
      executeHelloWorldPathVariableService(name) {
-        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`);
+        let username = 'user'
+        let password = 'password1234'
+
+        let basicAuthHeader = 'Basic' + window.btoa(username + ":" + password)
+
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`,
+        {
+            headers: {
+                authorization: basicAuthHeader
+            }
+        });
+
          //console.log('executed service')
      }
 
