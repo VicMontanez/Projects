37c1b1bb7660d2f947b866df3b4b1ca13381073a
unknown <v_montanez@hotmail.com>
2020-07-09 22:53:52 -0400

    renamed list name and objects

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicHardcodedService.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/music/MusicHardcodedService.java
@@ -11,33 +11,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class MusicHardcodedService {
 	
-	private static List<Music> music = new ArrayList<Music>();
+	private static List<Music> musics = new ArrayList<Music>();
 	private static int idCounter = 0;
 	
 	static {
-		music.add(new Music(++idCounter, "username","Drake", new Date(), false ));
-		music.add(new Music(++idCounter, "username","Snoop Dogg", new Date(), false ));
-		music.add(new Music(++idCounter, "username","Jay-z", new Date(), false ));
+		musics.add(new Music(++idCounter, "username","Drake", new Date(), false ));
+		musics.add(new Music(++idCounter, "username","Snoop Dogg", new Date(), false ));
+		musics.add(new Music(++idCounter, "username","Jay-z", new Date(), false ));
 	}
 	
 	public List<Music> findAll() {
-		return music;
+		return musics;
+		
 		
 	}
+	
 	
 	public Music deletebyId(long id) {
 		Music artist = findById(id);
 		
 		if(artist==null) return null;
 		
-		if(music.remove(artist)) {
+		if(musics.remove(artist)) {
 		return artist;
 	}
 		return null;
 	}
 
-	private Music findById(long id) {
-		for(Music artist:music) {
+	public Music findById(long id) {
+		for(Music artist:musics) {
 			if(artist.getId() == id) {
 				return artist;
 			}
