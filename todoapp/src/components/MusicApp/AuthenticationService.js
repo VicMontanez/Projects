c08de83d6e7ce6c31e2b625ff11a037d011c7ebe
unknown <v_montanez@hotmail.com>
2020-07-14 21:17:25 -0400

    used basic auth to connect to front end to authenticate user

--- todoapp/src/components/MusicApp/AuthenticationService.js
+++ todoapp/src/components/MusicApp/AuthenticationService.js
@@ -1,15 +1,22 @@
 import axios from 'axios'
 
 class AuthenticationService {
-    registerSuccessfulLogin(username, password) {
-        let username = 'username'
-        let password = 'password1234'
+    
+    executeBasicAuthenticationService(username, password) {
+        return axios.get(`http://localhost:8080/basicauth`, {headers: {authorization: this.createBasicAuthToken(username, password)}})
+    }
 
-        let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
-       
-        console.log('Successful Login')
+    createBasicAuthToken(username, password) {
+        return 'Basic ' + window.btoa(username + ":" + password)
+    }
+
+    registerSuccessfulLogin(username, password) {
+
+        //let basicAuthHeader = 'Basic ' + window.btoa(username + ":" + password)
+
+       // console.log('Successful Login')
         sessionStorage.setItem('authenticatedUser', username);
-        this.setupAxiosInterceptors(basicAuthHeader)
+        this.setupAxiosInterceptors(this.createBasicAuthToken(username, password))
     }
 
     logout() {
@@ -29,7 +36,8 @@ class AuthenticationService {
     }
 
     setupAxiosInterceptors(basicAuthHeader) {
-        
+       
+
         
         axios.interceptors.request.use(
             (config) => {
