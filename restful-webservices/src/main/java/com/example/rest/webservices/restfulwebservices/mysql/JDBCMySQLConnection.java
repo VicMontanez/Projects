eab63a779ada4f4391017589aa2371a4f8b54f74
unknown <v_montanez@hotmail.com>
2020-08-05 22:13:44 -0400

    added statement and showed hardcoded databse data

--- restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/mysql/JDBCMySQLConnection.java
+++ restful-webservices/src/main/java/com/example/rest/webservices/restfulwebservices/mysql/JDBCMySQLConnection.java
@@ -1,12 +1,15 @@
 package com.example.rest.webservices.restfulwebservices.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class JDBCMySQLConnection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String url = "jdbc:mysql://localhost:3306/namdb";
-		String username = "root";
+		String url = "jdbc:mysql://localhost:3306/musiclist";
+		String username = "db_admin";
 		String password = "";
 	
 		try {
@@ -16,11 +19,30 @@ public class JDBCMySQLConnection {
 		
 			
 			System.out.println("Connection successful!!!");
-			myConn.close();
-		}
-		catch(Exception exc) {
-			exc.printStackTrace();
-		}
-}
-	}
+			
+			// Create a statement
+						Statement myStmt = myConn.createStatement();
+			
+						// inserting data to MySQL
+//						String sql = "insert into stats "
+//								+ " (firstName, lastName, position, team, pointsPerGame)"
+//								+ " values ('Walker', 'Kemba', 'PG', 'BOS', '13.5')";
+						
+//						myStmt.executeUpdate(sql);
+						
+						// Execute SQL Query
+						ResultSet result = myStmt.executeQuery("select * from music");
+						
+						// Process the result set
+						while (result.next()) {
+							System.out.println(result.getString("artistName") + " " + result.getString("songName") + " " + result.getString("genre") + " " + result.getString("albumName"));
+						}
+						myConn.close();
+					}
+					catch(Exception exc) {
+						exc.printStackTrace();
+					}
 
+				}
+
+			}
